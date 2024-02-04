2cc1946c35b2872626eb66fb6fc4e9cb66eaa288
workwithraymond <workwithraymond01@gmail.com>
2024-02-03 20:25:55 -0500

    new commit

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,6 +1,18 @@
-import './fetchProjects';
+import {useFetchProjects} from './fetchProjects';
 
 const Project = () => {
+  const {loading, projects} = useFetchProjects();
+
+
+  if(loading) {
+    return (
+      <section className='projects'>
+        <h2>Loading...</h2>
+      </section>
+    )
+  }
+
+  console.log(loading, projects);
   return (
     <h2>Project</h2>
   )
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { createClient } from 'contentful';
 
 const client = createClient({
@@ -6,5 +7,32 @@ const client = createClient({
     accessToken: '8Zl7YNbZ38WHO2IEbYVRJE0LJqR3DNQ0PzjZVO0-gXk',
 });
 
-client.getEntries({content_type: 'projects' })
-.then((response) => console.log(response));
+export const useFetchProjects = () => {
+    const [loading, setLoading] = useState(true)
+    const [projects, setProjects] = useState([])
+
+    const getData = async() => {
+        try {
+            const response = await client.getEntries({ content_type: 'projects' });
+            const projects = response.items.map((item) => {
+                const {title, url, image} = item.fields
+                const id = item.sys.id
+                const img = image?.fields?.file?.url
+                return {title, url, id, img}
+
+            });
+            setProjects(projects);
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    },[]);
+    return { loading, projects}
+
+}
+
