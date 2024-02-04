3757a39766d44a7444d5787637fcbf55c012ef51
workwithraymond <workwithraymond01@gmail.com>
2024-02-03 20:40:02 -0500

    new commit

--- src/Project.jsx
+++ src/Project.jsx
@@ -12,9 +12,23 @@ const Project = () => {
     )
   }
 
-  console.log(loading, projects);
+  
   return (
-    <h2>Project</h2>
+    <section className='projects'>
+    <div className='title'>
+      <h2>projects</h2>
+      <div className='title-underline'></div>
+    </div>
+    <div className='projects-center'>
+      {projects.map((project) => {
+        const {id, img, url, title} = project 
+        return <a key={id} href={url} target='_blank' rel='noreferrer' className='project'>
+          <img src={img} alt={title} className='img' />
+          <h5>{title}</h5>
+        </a>
+      })}
+    </div>
+  </section>
   )
 }
 export default Project
